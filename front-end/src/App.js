ef5e4e1bebb7457561149da66c2555a0202efd5d
Jayden Lee <Jayden@rlc.com.au>
2020-08-16 15:14:35 +1000

    Sample Upload for Pi

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react';
+
+import "./App.css";
+import HomeScreen from './Pages/HomeScreen';
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+  } 
+from "react-router-dom";
+
+
+
+// a in style.css has now become <link> so remember to replace that in the CSS. 
+
+class App extends Component {
+    
+    render() {
+        return (
+            <Router>
+                <Switch>
+                    <Route path="/">
+                      <HomeScreen/>
+                     </Route>
+                    <Route path="/Indicators">
+                      <h1>Test</h1>
+                    </Route>
+                    <Route path="/AboutUs">
+                      <h1>Test</h1>
+                    </Route>
+                    <Route path="/ContactUs">
+                      <h1>Test</h1>
+                    </Route>
+                </Switch>
+            </Router>
+        )
+    }
+};
+
+
 
 export default App;
---
+++ front-end/src/Pages/HomeScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class HomeScreen extends Component {
+    render () {
+        return(
+            <div>
+                Hello!
+            </div>
+        );
+    }
+}
+
+export default HomeScreen;
