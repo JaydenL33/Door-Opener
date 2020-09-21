ccc24d50e77387b92b5e294d041191c422ce9492
Jayden Lee <Jayden@rlc.com.au>
2020-09-21 14:36:11 +1000

    Update sampleBackend.ino

--- backend/sampleBackend/sampleBackend.ino
+++ backend/sampleBackend/sampleBackend.ino
@@ -4,11 +4,19 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600); 
   pinMode(A0, OUTPUT);
   digitalWrite(A0, HIGH);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    incomingByte = Serial.read();
 
+    // say what you got:
+    Serial.print("I received: ");
+    Serial.println(incomingByte, DEC);
+  }
 }
