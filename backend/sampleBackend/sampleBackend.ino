bb87174c6208fe9be767d5d11d0d71ae3b505a05
Jayden Lee <Jayden@rlc.com.au>
2020-09-21 10:57:44 +1000

    Create sampleBackend.ino

---
+++ backend/sampleBackend/sampleBackend.ino
@@ -0,0 +1,14 @@
+// Pin assignments
+
+# define LED_1 A0
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(A0, OUTPUT);
+  digitalWrite(A0, HIGH);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
