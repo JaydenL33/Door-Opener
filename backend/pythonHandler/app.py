8984751af078fae1987422477bcfb61aeea87f65
Jayden Lee <Jayden@rlc.com.au>
2020-09-21 14:56:41 +1000

    Changes to run

--- backend/pythonHandler/app.py
+++ backend/pythonHandler/app.py
@@ -1,13 +1,13 @@
 # Version 3.1
 # Created by Aaron Su 16 Dec 2019
-# Modified by Jayden Lee 15/04/2020
+# Modified by Jayden Lee 21/09/2020
 
 from src import create_app
 
 server = create_app()
 
-Local = False
+Local = True
 if Local == False:
-    server.run(debug=False, host='134.122.104.123')
+    server.run(debug=False, host='', port=3030)
 else:
-    server.run(debug=False, host='localhost')
+    server.run(debug=False, host='localhost', port=3030)
