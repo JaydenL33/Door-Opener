8095f7677e160d5257aa4f36e30bf3b73c60ac8f
Jayden Lee <Jayden@rlc.com.au>
2020-09-21 14:52:19 +1000

    Update routes.py
    Fixed Indentation

--- backend/pythonHandler/src/routes.py
+++ backend/pythonHandler/src/routes.py
@@ -29,24 +29,24 @@ POST api://test
 body        application/json
 -----------------------
 {
-    NULL
+	NULL
 }
 
 
 returns     application/json
 -----------------------
 {
-    successful: bool,
-    totalhits: int,
-    date: str,
-    text: [
-        {
-            name: str,
-            count: int,
+	successful: bool,
+	totalhits: int,
+	date: str,
+	text: [
+		{
+			name: str,
+			count: int,
 
-        }
-    ]
-    
+		}
+	]
+	
 }
 
 
@@ -54,14 +54,14 @@ returns     application/json
 
 @api.route("/", methods=["GET"])
 def testFunction():
-    ser = serial.serial('/dev/ttyACMO0')
-    ser.write(b'a')
+	ser = serial.serial('/dev/ttyACMO0')
+	ser.write(b'a')
 
-    while(ser.read() != 'w'):
-        pass
+	while(ser.read() != 'w'):
+		pass
 
-    ser.write(b'b')
-    ser.close()
+	ser.write(b'b')
+	ser.close()
 
 
 
@@ -70,59 +70,58 @@ def testFunction():
 	
 	return Response(res, status=200, mimetype="application/json")
 
- 
 
 
 @api.route("/data", methods=["GET"])
 def dataCall():
-    # req = request.json
+	# req = request.json
  
-    res = {
-    "successful": True,
-    "data": [],
-    }
+	res = {
+	"successful": True,
+	"data": [],
+	}
  
-    res["data"] = puller()
+	res["data"] = puller()
  
-    # for i in range(len(constructedData)):
-    #   res["data"].append(constructedData[i])
+	# for i in range(len(constructedData)):
+	#   res["data"].append(constructedData[i])
  
-    res = json.dumps(res) # json serializer
-    return Response(res, status=200, mimetype="application/json")
+	res = json.dumps(res) # json serializer
+	return Response(res, status=200, mimetype="application/json")
  
  
 
 
 def puller():
-    """Write records stored in a DataFrame to a SQL database.
+	"""Write records stored in a DataFrame to a SQL database.
 
-    Databases supported by SQLAlchemy are supported.
-    Tables can be newly created, appended to, or overwritten.
+	Databases supported by SQLAlchemy are supported.
+	Tables can be newly created, appended to, or overwritten.
 
-    see: pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
-    """
-    def dbConnect():
-        # pull sensitive settings from local.env for database login
-        env = environConfig.safe_environ()
-        URI_str = env("DB_URI")
-        engine = db.create_engine(URI_str)
-        return engine
-    
-    # create a session with our engine
-    engine = dbConnect()
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    session = Session()
-    # connect the engine
-    connection = engine.connect()
-    # create a query with the current session
-    # Create a definition of the existing table for sqlalchemy
-    COVID19 = db.Table("COVID19", db.MetaData(), autoload=True, autoload_with=engine)
-    queryset = session.query(COVID19).limit(100).all()
-    # queryset = db.select([BP_06]).limit(100)
-    # ResultProxy = connection.execute(queryset)
+	see: pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
+	"""
+	def dbConnect():
+		# pull sensitive settings from local.env for database login
+		env = environConfig.safe_environ()
+		URI_str = env("DB_URI")
+		engine = db.create_engine(URI_str)
+		return engine
+	
+	# create a session with our engine
+	engine = dbConnect()
+	Session = sessionmaker()
+	Session.configure(bind=engine)
+	session = Session()
+	# connect the engine
+	connection = engine.connect()
+	# create a query with the current session
+	# Create a definition of the existing table for sqlalchemy
+	COVID19 = db.Table("COVID19", db.MetaData(), autoload=True, autoload_with=engine)
+	queryset = session.query(COVID19).limit(100).all()
+	# queryset = db.select([BP_06]).limit(100)
+	# ResultProxy = connection.execute(queryset)
 
-    return queryset
+	return queryset
 
 # @api.route("/")
 # def serverCheck():
