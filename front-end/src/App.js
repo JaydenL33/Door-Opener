8df48fcdfac53388f2efcb78bfbae1240a50295d
Jayden Lee <Jayden@rlc.com.au>
2020-10-05 13:29:03 +1100

    Updated to reflected Login Page
    Based projected on a sample login paged but obviously will reflect current work with login pages

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,42 +1,26 @@
-
 import React, { Component } from 'react';
-
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
 import "./App.css";
-import HomeScreen from './Pages/HomeScreen';
-import {
-	BrowserRouter as Router,
-	Switch,
-	Route,
-  } 
-from "react-router-dom";
+import Routes from "./Routes";
 
 
-
-// a in style.css has now become <link> so remember to replace that in the CSS. 
-
-class App extends Component {    
-    render() {
-        return (
-            <Router>
-                <Switch>
-                    <Route path="/">
-                      <HomeScreen/>
-                     </Route>
-                    <Route path="/Indicators">
-                      <h1>Test</h1>
-                    </Route>
-                    <Route path="/AboutUs">
-                      <h1>Test</h1>
-                    </Route>
-                    <Route path="/ContactUs">
-                      <h1>Test</h1>
-                    </Route>
-                </Switch>
-            </Router>
-        )
-    }
+class App extends Component {   
+  render() { 
+  return (
+    <div className="App container">
+      <Navbar fluid collapseOnSelect>
+        <Navbar.Header>
+          <Navbar.Brand>
+            <Link to="/">Scratch</Link>
+          </Navbar.Brand>
+          <Navbar.Toggle />
+        </Navbar.Header>
+      </Navbar>
+      <Routes />
+    </div>
+  )
+  }
 };
-
-
 
 export default App;
