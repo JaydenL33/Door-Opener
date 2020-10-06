72caec803dba791df0832d9d5376f8a315ec9f72
Jayden Lee <Jayden@rlc.com.au>
2020-10-06 20:37:01 +1100

    Changed to HTTP://

--- front-end/src/containers/Login.js
+++ front-end/src/containers/Login.js
@@ -17,7 +17,7 @@ export default function Login() {
       email: email,
     };
     
-    axios.post('localhost:3030/', user)
+    axios.post('http://localhost:3030/', user)
     .then(function (response) {
       console.log(response);
     })
