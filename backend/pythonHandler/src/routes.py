659de0ca665d9398c9c607b7ca73c756d603c5e3
Jayden Lee <Jayden@rlc.com.au>
2020-10-06 20:47:40 +1100

    Update routes.py

--- backend/pythonHandler/src/routes.py
+++ backend/pythonHandler/src/routes.py
@@ -14,44 +14,8 @@ api = Blueprint("api", __name__)
 
 
 
-"""
-Route: ("IPADDR:3000/test")
-Function: The function first takes the request from request.json to process the GETREQUEST, which should be null.
 
-
-## Test Function
-
-```
-POST api://test
-
-
-body        application/json
------------------------
-{
-	NULL
-}
-
-
-returns     application/json
------------------------
-{
-	successful: bool,
-	totalhits: int,
-	date: str,
-	text: [
-		{
-			name: str,
-			count: int,
-
-		}
-	]
-	
-}
-
-
-"""
-
-@api.route("/", methods=["POST"])
+@api.route("/", methods=["GET"])
 def testFunction():
 
 	ser = serial.Serial('/dev/ttyACMO0')
@@ -86,38 +50,3 @@ def dataCall():
  
 
 
-# def puller():
-# 	"""Write records stored in a DataFrame to a SQL database.
-
-# 	Databases supported by SQLAlchemy are supported.
-# 	Tables can be newly created, appended to, or overwritten.
-
-# 	see: pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
-# 	"""
-# 	def dbConnect():
-# 		# pull sensitive settings from local.env for database login
-# 		env = environConfig.safe_environ()
-# 		URI_str = env("DB_URI")
-# 		engine = db.create_engine(URI_str)
-# 		return engine
-	
-# 	# create a session with our engine
-# 	engine = dbConnect()
-# 	Session = sessionmaker()
-# 	Session.configure(bind=engine)
-# 	session = Session()
-# 	# connect the engine
-# 	connection = engine.connect()
-# 	# create a query with the current session
-# 	# Create a definition of the existing table for sqlalchemy
-# 	COVID19 = db.Table("COVID19", db.MetaData(), autoload=True, autoload_with=engine)
-# 	queryset = session.query(COVID19).limit(100).all()
-# 	# queryset = db.select([BP_06]).limit(100)
-# 	# ResultProxy = connection.execute(queryset)
-
-# 	return queryset
-
-# @api.route("/")
-# def serverCheck():
-#     """Return a friendly HTTP greeting."""
-#     return "Server Running"
